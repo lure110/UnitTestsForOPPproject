ca31ced8c90043cc223bf803b9cf0e78b08b2457
NaktinisApuokas <airidas.setkus@ktu.edu>
2022-11-07 16:06:11 +0200

    test
    test

---
+++ Patterns/PowerTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using csgame_backend.Patterns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using csgame_backend.Data.Entities;
+using Assert = Xunit.Assert;
+
+namespace csgame_backend.Patterns.Tests
+{
+    public class PowerTests
+    {
+        IBuilder? _builder;
+
+        [Fact]
+        public void dmgUpTest()
+        {
+            _builder = new Builder();
+            _builder.AddGunPistol();
+            Player player = _builder.Build();
+            Player mockplayer = _builder.Build();
+
+            foreach (Gun gun in player.guns)
+            {
+                gun.Damage += 5;
+            }
+            bool same = player.guns.Equals(mockplayer.guns);
+
+            Assert.False(same, "first players guns: " + mockplayer.guns + " should be diferent as player: " + player.guns);
+        }
+
+        [Fact]
+        public void dmgSuperUpTest()
+        {
+            _builder = new Builder();
+            _builder.AddGunPistol();
+            Player player = _builder.Build();
+            Player mockplayer = _builder.Build();
+
+            foreach (Gun gun in player.guns)
+            {
+                gun.Damage *= 5;
+            }
+            bool same = player.guns.Equals(mockplayer.guns);
+
+            Assert.False(same, "first players guns: " + mockplayer.guns + " should be diferent as player: " + player.guns);
+        }
+    }
+}
---
+++ Patterns/UtilityTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using csgame_backend.Patterns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using csgame_backend.Data.Entities;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace csgame_backend.Patterns.Tests
+{
+    public class UtilityTests
+    {
+        IBuilder? _builder;
+        [Fact]
+        public void hlthUpTest()
+        {
+            _builder = new Builder();
+            _builder.AddGunPistol();
+            Player player = _builder.Build();
+            Player mockplayer = new Player("Unit_Test_Player", 0, 0);
+            mockplayer.Health = 250;
+
+            player.Health += 150;
+
+            bool same = player.Health.Equals(mockplayer.Health);
+
+            Assert.True(same, "first players Health: " + mockplayer.Health + " should be same as player: " + player.Health);
+        }
+
+        [Fact]
+        public void hlthDownTest()
+        {
+            _builder = new Builder();
+            _builder.AddGunPistol();
+            Player player = _builder.Build();
+            Player mockplayer = new Player("Unit_Test_Player", 0, 0);
+            mockplayer.Health = 70;
+
+            player.Health -= 30;
+
+            bool same = player.Health.Equals(mockplayer.Health);
+
+            Assert.True(same, "first players Health: " + mockplayer.Health + " should be same as player: " + player.Health);
+        }
+
+    }
+}
